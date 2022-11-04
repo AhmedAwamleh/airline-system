17d9649fd7609ee8169fbbd1c7638d6aaeb4ed42
Ahmed AL Awamleh <awamlehad@icloud.com>
2022-11-04 23:18:40 +0200

    fix it

--- system.js
+++ system.js
@@ -1,13 +1,34 @@
-require('./manager');
-require('./pilot');
-const events = require('./events');
+const events = require("./events")
 const { faker } = require('@faker-js/faker');
-
+require('./manager')
+require('./pilot')
 
 events.on('new-flight', (data) => {
   console.log(`Flight {
   event: 'new-flight',
-  time:  ${faker.date.past()},
+  time:  ${new Date().toUTCString()},
+  Details: {
+  airLine: ${data.airLine},
+  destination: ${data.destination},
+  pilot: ${data.pilot},
+  flightID: ${data.flightID}
+}`)
+})
+events.on('took-off', (data) => {
+  console.log(`took-off {
+  event: 'took-off',
+  time:  ${new Date().toUTCString()},
+  Details: {
+  airLine: ${data.airLine},
+  destination: ${data.destination},
+  pilot: ${data.pilot},
+  flightID: ${data.flightID}
+}`)
+})
+events.on('arrived', (data) => {
+  console.log(`arrived {
+  event: 'arrived',
+  time:  ${new Date().toUTCString()},
   Details: {
   airLine: ${data.airLine},
   destination: ${data.destination},
@@ -16,26 +37,3 @@ events.on('new-flight', (data) => {
 }`)
 })
 
-events.on('took-off', (data) => {
-  console.log(`Flight {
-      event: 'took_off',
-      time:  ${faker.date.past()},)},
-      Details: {
-      airLine: ${data.airline},
-      destination: ${data.destination},
-      pilot: ${data.pilot},
-      flightID: ${data.flightID}
-  }`)
-})
-
-events.on('arrived', (data) => {
-  console.log(`Flight {
-      event: 'arrived',
-      time:  ${faker.date.past()},)},
-      Details: {
-      airLine: ${data.airline},
-      destination: ${data.destination},
-      pilot: ${data.pilot},
-      flightID: ${data.flightID}
-  }`)
-})
